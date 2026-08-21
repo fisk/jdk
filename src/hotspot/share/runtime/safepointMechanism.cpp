0561c713e0e5f893e3ab28a57321128d9689e7c9
Erik Österlund <eosterlund@openjdk.org>
2026-08-21 14:13:51 +0000

    Improve asserts

--- src/hotspot/share/runtime/safepointMechanism.cpp
+++ src/hotspot/share/runtime/safepointMechanism.cpp
@@ -94,8 +94,8 @@ uintptr_t SafepointMechanism::compute_poll_word(bool armed, uintptr_t stack_wate
 void SafepointMechanism::update_poll_values(JavaThread* thread) {
   assert(thread == Thread::current(),
          "only the owning thread may recompute its poll values");
-  //assert(thread->thread_state() != _thread_blocked, "Must not be");
-  //assert(thread->thread_state() != _thread_in_native, "Must not be");
+  assert(thread->thread_state() != _thread_blocked, "Must not be");
+  assert(thread->thread_state() != _thread_in_native, "Must not be");
 
   for (;;) {
     bool armed = has_pending_safepoint(thread);
